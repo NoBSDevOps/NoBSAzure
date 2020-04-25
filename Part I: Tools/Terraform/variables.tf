045a06ddb9a2c39c03ee60595e9a8ebb8c677586
Adam Bertram <adbertram@gmail.com>
2020-04-25 11:19:18 -0500

    Create variables.tf

---
+++ Part I: Tools/Terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "subscriptionID" { 
+    type = string 
+    description = "Variable for our resource group"
+}
+
+variable "resourceGroupName" {
+    type = string    
+    description = "name of resource group"
+}
+
+variable "location" {    
+    type = string    
+    description = "location of your resource group"
+
+}
