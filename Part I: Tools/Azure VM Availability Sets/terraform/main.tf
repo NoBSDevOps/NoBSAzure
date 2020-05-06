15daf5814011ddb6c45cdc38b94f138a0ea0e2ea
Adam Bertram <adbertram@gmail.com>
2020-05-06 10:54:52 -0500

    updates

--- Part I: Tools/Azure VM Availability Sets/terraform/main.tf
+++ Part I: Tools/Azure VM Availability Sets/terraform/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "=2.5.0"
+  version = "=2.8.0"
   features {}
 }
 
@@ -35,7 +35,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "10.0.2.0/24"
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_availability_set" "availset" {
@@ -61,6 +61,19 @@ resource "azurerm_network_interface" "vnic" {
   }
 }
 
+resource "azurerm_network_interface_security_group_association" "nsg-assoc" {
+  count                     = 5
+  network_interface_id      = element(azurerm_network_interface.vnic.*.id, count.index)
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "vnicToBePool" {
+  count                   = 5
+  network_interface_id    = element(azurerm_network_interface.vnic.*.id, count.index)
+  ip_configuration_name   = "internal"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.lb-backendpool.id
+}
+
 resource "azurerm_windows_virtual_machine" "vm" {
   name                  = "vm-${count.index}"
   count                 = 5
@@ -83,4 +96,69 @@ resource "azurerm_windows_virtual_machine" "vm" {
     sku       = "2019-Datacenter"
     version   = "latest"
   }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "rdp-access"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "rdp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "pubIp" {
+  name                = "${var.appName}-pubIp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "${var.appName}-lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  frontend_ip_configuration {
+    name                 = "${var.appName}-feip"
+    public_ip_address_id = azurerm_public_ip.pubIp.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "lb-backendpool" {
+  resource_group_name = azurerm_resource_group.rg.name
+  loadbalancer_id     = azurerm_lb.lb.id
+  name                = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_probe" "lb-probe" {
+  resource_group_name = azurerm_resource_group.rg.name
+  loadbalancer_id     = azurerm_lb.lb.id
+  name                = "RDP-available"
+  port                = 3389
+}
+
+resource "azurerm_lb_rule" "lb-rule" {
+  resource_group_name            = azurerm_resource_group.rg.name
+  loadbalancer_id                = azurerm_lb.lb.id
+  name                           = "LBRule"
+  protocol                       = "Tcp"
+  frontend_port                  = 3389
+  backend_port                   = 3389
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.lb-backendpool.id
+  frontend_ip_configuration_name = "${var.appName}-feip"
+}
+
+output "pubip" {
+  value       = azurerm_public_ip.pubIp.ip_address
+  description = "Public IP of the load balancer"
 }
