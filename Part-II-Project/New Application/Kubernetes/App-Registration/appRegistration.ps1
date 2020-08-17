a7834eceee5df27757d3d948bc0bee2c197907bf
Michael Levan <mlevan1992@gmail.com>
2020-08-17 07:49:06 -0400

    App registration code

---
+++ Part-II-Project/New Application/Kubernetes/App-Registration/appRegistration.ps1
@@ -0,0 +1,16 @@
+# This code does two things:
+# 1. Creates a new app registration
+# 2. Takes the client ID and password, then stores them in Key Vault
+
+param(
+    [string]$name = "NoBS",
+    [string]$rgName = "NoBS"
+)
+
+$account = az account show | ConvertFrom-Json | Select -ExpandProperty id
+
+$appReg = az ad sp create-for-rbac -n $name `
+                                   --role contributor `
+                                   --scopes "subscriptions/$account/resourceGroups/$rgName"
+
+$appReg | ConvertFrom-Json
