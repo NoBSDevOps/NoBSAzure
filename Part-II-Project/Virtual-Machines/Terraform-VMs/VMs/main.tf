a4a37386059cb0c540d21ea959e633ac917b82f0
Adam Bertram <adbertram@gmail.com>
2020-10-18 12:25:54 -0500

    Update main.tf

--- Part-II-Project/Virtual-Machines/Terraform-VMs/VMs/main.tf
+++ Part-II-Project/Virtual-Machines/Terraform-VMs/VMs/main.tf
@@ -1,8 +1,5 @@
 provider "azurerm" {
   version = "2.0.0"
-  storage_account_name  = "nobsstorage"
-  container_name        = "tstate"
-  key                   = "terraform.tfstate"
   features {}
 }
 
