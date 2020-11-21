c84c410b5aff824b435161fa179272a2638de793
Adam Bertram <adbertram@gmail.com>
2020-11-21 13:44:42 -0600

    Update main.tf

--- Part-II-Project/Virtual-Machines/Terraform-VMs/VMs/main.tf
+++ Part-II-Project/Virtual-Machines/Terraform-VMs/VMs/main.tf
@@ -141,7 +141,7 @@ resource "azurerm_lb_backend_address_pool" "be_pool" {
   
 resource "azurerm_network_interface_backend_address_pool_association" "be_assoc" {
   count                   = 2
-  network_interface_id    = azurerm_network_interface.main.[count.index].id
+  network_interface_id    = azurerm_network_interface.main[count.index].id
   ip_configuration_name   = "ip_config_${count.index}"
   backend_address_pool_id = azurerm_lb_backend_address_pool.be_pool.id
 }
